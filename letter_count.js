8207c79b01af4794a265f6b57f73e045d62521c2
Alexander Abushady <aabushady214@gmail.com>
2019-09-08 16:24:22 -0400

    Update letter_count.js
    Added problem statement.

---
+++ letter_count.js
@@ -0,0 +1,17 @@
+/*
+Write a program that counts and displays the number of times a specified
+character appears in a string. Use the following:
+
+Input String: "As he crossed toward the pharmacy at the corner he
+involuntarily turned his head because of a burst of light that had ricocheted
+from his temple, and saw, with that quick smile with which we greet a
+rainbow or a rose, a blindingly white parallelogram of sky being unloaded
+from the van-a dresser with mirrors across which, as across a cinema
+screen, passe a flawlessly clear reflection of boughs sliding and swaying
+not arboreally, but with a human vacillation, produced by the nature of
+those who were carrying this sky, these boughs, this glding facade."
+
+Character to Count: S
+
+THe program should count lowercase and uppercase instances of the character.
+*/
